6e07f87b2ca89a34d88f974951b7a820f2c0724d
pikapotter <73731967+pikapotter@users.noreply.github.com>
2020-10-31 02:11:16 -0400

    Create local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,12 @@
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
